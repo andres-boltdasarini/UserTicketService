a99d2c01b7d66a5e5ad9b77da5f90d709b39b9a6
akupriyanov <akupriyanov@astralinux.ru>
2025-05-27 15:12:28 +0300

    1661_fin

--- UserTicketService.Tests/UnitTest1.cs
+++ UserTicketService.Tests/UnitTest1.cs
@@ -1,31 +1,37 @@
-using Moq;
-
-[TestFixture]
-public class UserRepositoryTests
+namespace UserTicketService.Tests
 {
-    [Test]
-    public void FindAllMustReturnCorrectValue()
+    [TestFixture]
+    public class Class1
     {
-        var list = new List<User> {
-      new User()
-      {
-        Name = "Антон"
-      },
-      new User()
-      {
-        Name = "Иван"
-      },
-      new User()
-      {
-        Name = "Алексей"
-      },
-    };
-        Mock<IUserRepository> mock = new Mock<IUserRepository>();
-
-        mock.Setup(v => v.FindAll()).Returns(list);
-
-        Assert.That(mock.Object.FindAll().Any(user => user.Name == "Антон"));
-        Assert.That(mock.Object.FindAll().Any(user => user.Name == "Иван"));
-        Assert.That(mock.Object.FindAll().Any(user => user.Name == "Алексей"));
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(300, 10) == 3000);
+        }
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(300, 10) == 30);
+        }
+        [Test]
+        public void Division_MustThrowException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
     }
 }
--- UserTicketService/Program.cs
+++ UserTicketService/Program.cs
@@ -1,34 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-    class Program
-    {
-
+﻿class Program
+{
         static void Main(string[] args)
         {
-
+        Calculator calculator = new Calculator();
+        Console.WriteLine(calculator.Subtraction(300, 10));
         }
-    }
-
-public class UserRepository : IUserRepository
-{
-    public IEnumerable<User> FindAll()
-    {
-        return null;
-    }
 }
-
-public interface IUserRepository
+public class Calculator
 {
-    IEnumerable<User> FindAll();
-}
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
 
-public class User
-{
-    public string Name
-    {
-        get;
-        set;
-    }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
 }
